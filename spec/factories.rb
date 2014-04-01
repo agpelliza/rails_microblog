10073c3b2a75c93e4440cd9c2a3bec1e7dc3d06f
agpelliza <agpelliza@gmail.com>
2014-04-01 11:26:18 -0300

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Agustin Pelliza"
+    email    "agpelliza@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
